57c72a7657558edb5e13162365b97f4011037a1e
yang <1471517131@qq.com>
2022-01-22 22:05:06 +0800

    fix:修改生成菜单函数

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -1,9 +1,4 @@
 import { privateRoutes, publicRoutes } from '@/router'
-// import {
-//   superAdminMenus,
-//   adminMenus,
-//   staffMenus
-// } from '@/router/testRoleRouter'
 
 export default {
   namespaced: 'permission',
@@ -19,33 +14,21 @@ export default {
   },
   actions: {
     async getMenus(context, menus) {
-      // 改造成test数据
-      // if (context.rootGetters.userInfo) {
-      //   const { userInfo } = context.rootGetters
-      //   if (userInfo.title === '超级管理员') {
-      //     menus = superAdminMenus
-      //   } else if (userInfo.title === '管理员') {
-      //     menus = adminMenus
-      //   } else {
-      //     menus = staffMenus
-      //   }
-      // }
       // 从定义的私有路由表里筛选出后端返回的指定路由表
       const privateRoutesArr = filterPrivateRoutes(menus, privateRoutes)
+      console.log(privateRoutesArr)
       privateRoutesArr.push({
         path: '/:pathMatch(.*)*',
         name: '404',
         component: () => import('@/views/error-page/404'),
         hidden: true
       })
-      console.log(privateRoutesArr)
 
       // 处理一下一级菜单问题（只有一个子路由的时候该子路由就作为一级菜单显示）
       const menuRoutersArr = getMenuRoutersArr([
         ...publicRoutes,
         ...privateRoutesArr
       ])
-      console.log(menuRoutersArr)
       // 赋值
       context.commit('changePermissionState', {
         key: 'menuRouters',
@@ -62,39 +45,24 @@ export default {
 }
 // 从定义的私有路由表里筛选出后端返回的指定路由表，后续去添加路由
 function filterPrivateRoutes(menus, privateRoutes) {
-  let routerArr = []
-  routerArr = privateRoutes.filter((itemRouter) => {
-    if (itemRouter.hidden) return true
-    return menus.some((itemMenus) => {
-      if (itemMenus.path === itemRouter.path) {
-        if (itemMenus.menuName && itemRouter.meta) {
-          itemRouter.meta.title = itemMenus.menuName
-        }
-        if (itemMenus.icon && itemRouter.meta) {
-          itemRouter.meta.icon = itemMenus.icon
-        }
-        if (itemMenus.children && itemMenus.children.length > 0) {
-          itemRouter.children = filterPrivateRoutes(
-            itemMenus.children,
-            itemRouter.children
-          )
-          // 经过筛选后发现后端返回children里只有需要菜单栏隐藏的路由，菜单栏会隐藏该路由
-          // if (itemRouter.children.every((item) => item.hidden)) {
-          //   itemRouter.hidden = true
-          // }
-        } else if (itemMenus.children && itemMenus.children.length === 0) {
-          // 防止显示一个无子集但是children为空数组的菜单   (super-admin账号的article)
-          itemRouter.hidden = true
-          // 删掉是为了防止去注册路由（代码定义的路由会被筛选到）
-          delete itemRouter.children
-        }
-        return true
-      } else {
-        return false
-      }
-    })
+  return menus.map((itemMenu) => {
+    const routeInfo = {}
+    const obj = privateRoutes.find(
+      (itemRoute) => itemRoute.path === itemMenu.path
+    )
+    Object.assign(routeInfo, obj)
+    if (itemMenu.menuName && routeInfo.meta) {
+      routeInfo.meta.title = itemMenu.menuName
+    }
+    if (itemMenu.icon && routeInfo.meta) {
+      routeInfo.meta.icon = itemMenu.icon
+    }
+    if (itemMenu.children) {
+      routeInfo.children = filterPrivateRoutes(itemMenu.children, obj.children)
+    }
+    // 就算itemMenu没有children，也要用find找到与之对应得本地路由（可能包含非菜单页面or 一级路由）
+    return routeInfo
   })
-  return routerArr
 }
 // 根的一级菜单一定要有children
 // 只处理一级菜单问题，嵌套的一级不在此（只有一个作为菜单子路由的时候该子路由就作为一级菜单显示）因为一级菜单有个需要对应layout的父组件
