4287ff90ccc518e0b1fb50249690029d0149f22f
yang <1471517131@qq.com>
2022-02-20 19:09:40 +0800

    feat:readme

--- README.md
+++ README.md
@@ -5,3 +5,13 @@
 
 本项目接口使用了 node+koa2+noSql，可以查看[csdn:node+koa+mongodb 搭建项目](https://blog.csdn.net/Angely_cc/article/details/122503329)
 接口代码：[github:mykoa](https://github.com/cc147151/mykoa)接口所用的表在 mykoa 项目里 collections 文件夹里
+
+# 该项目特点：
+
+### 左边菜单栏是根据用户角色动态返回，内含页面内按钮级别权限。
+
+### 用户管理(新增编辑用户)
+
+### 菜单管理(左边菜单栏增删改查)
+
+### 角色列表(添加角色，分配权限)
--- vue.config.js
+++ vue.config.js
@@ -6,13 +6,15 @@ const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
 module.exports = {
+  productionSourceMap: false,
+  publicPath: './',
   devServer: {
     // 配置反向代理
     proxy: {
       // 当地址中有/api的时候会触发代理机制
       '/api': {
         // 要代理的服务器地址  这里不用写 api
-        target: 'http://192.168.0.106:3000',
+        target: 'http://192.168.0.105:3000',
         changeOrigin: true // 是否跨域
         // 后端接口是'https:xxx/api/xxx',需要/api这个字段，所以不能用下面代码
         // pathRewrite:{
